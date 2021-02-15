222916173ac254b7407f85c21e99d34dbecfc71d
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-02-15 22:32:45 +0100

    add chapter 1

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+
+            int a = 10;
+
+            a++;
+            Console.WriteLine($"Valeur de a: {a}");
+            Console.WriteLine($"Valeur de a négatif: {-a}");
+
+            Console.ReadKey(true);
+        }
+    }
+}
