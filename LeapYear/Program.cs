bfd81fe9ffea220a92ebfbce7a4e514e4699727f
Nicolas Wailly <nicolas.wailly.dev@outlook.com>
2021-02-15 22:33:21 +0100

    add chapter 3

---
+++ LeapYear/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeapYear
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i = 2021; i < 2042; i++)
+            {
+                if(i%4 == 0 && (i%100 != 0 || i%400 ==0))
+                {                   
+                    Console.WriteLine($"{i} is a leap year");                    
+                }
+                else
+                {
+                    Console.WriteLine($"{i} is not a leap year");
+                }
+            }
+
+            Console.WriteLine();
+
+            // use of IsLeapYear
+            for (int i = 2021; i < 2042; i++)
+            {
+                if(DateTime.IsLeapYear(i))
+                    Console.WriteLine($"{i} is a leap year");
+            }
+
+            Console.ReadKey(true);
+        }
+    }
+}
